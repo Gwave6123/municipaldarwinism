3cc31aed1bc24e86c5d8e7b17c1c0a903d6fd285
Gwave6123 <Nickse66@gmail.com>
2017-06-14 15:14:24 -0400

    v0.1

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+function loadcheck(){
+	if (document.cookie.indexOf("municipaldarwinism=") >= 0){
+		window.alert("Found Cookies");
+		
+		return 1;
+
+	}
+	else {
+		window.alert("No Cookies");
+		return 0;
+	}
+}
+
+function newgame(){
+	var expire = new Date;
+	expire.setDate(expire.getDate()+14);
+	document.cookie = "username=municipaldarwinism; expires=" + expire + "; path=/";
+
+}
+function main(){
+	if (loadcheck() == 0){
+		newgame();
+		
+	}
+	else {
+		
+	}
+
+	
+}
